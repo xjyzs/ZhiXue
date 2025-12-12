1b75eb0f2e49f55c014c7d21789a298123a1f475
xjyzs <158408146+xjyzs@users.noreply.github.com>
2025-12-12 09:35:56 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0-NewSemester"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         androidResources.localeFilters += listOf("zh")
